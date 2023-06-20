526f0093f8f5ad17161e15275a1dff88098ec156
Aidin Bekturganov <bekturganovaidin000@gmail.com>
2023-06-20 13:16:51 +0600

    fix : fixed model and dbcontext

--- TestTask.Database/ApplicationDbContext.cs
+++ TestTask.Database/ApplicationDbContext.cs
@@ -13,4 +13,13 @@ public class ApplicationDbContext : IdentityDbContext
     }
     
     public DbSet<Employee> Employees { get; set; }
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<ProjectEmployee> ProjectEmployees { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<ProjectEmployee>()
+            .HasKey(x => new { x.EmployeeId, x.ProjectId, x.ManagerId });
+    }
 }
--- TestTask.Domain/Models/Employee.cs
+++ TestTask.Domain/Models/Employee.cs
@@ -2,6 +2,7 @@ namespace TestTask.Domain.Models;
 
 public class Employee
 {
+    public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string MiddleName { get; set; }
